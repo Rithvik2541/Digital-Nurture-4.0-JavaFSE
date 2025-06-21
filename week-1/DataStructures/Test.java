33e539d484755ea2f26a2f3c6f9018ab5f81175c
Rithvik2541 <rithvikvarmna2541@gmail.com>
2025-06-22 00:52:48 +0530

    from intellij-2

--- week-1/DataStructures/Test.java
+++ week-1/DataStructures/Test.java
@@ -1,5 +1,5 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Welcome oto the Tsst asa!");
+        System.out.println("Welcome asto the Tsst asa!");
     }
 }
